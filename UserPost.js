85a3fa77f4c9bb69021dca9bf171e741cda48a51
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2023-10-27 21:29:34 +0530

    getposts done

--- UserPost.js
+++ UserPost.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { Post } = require("./Schema/PostsSchema");
+const { User } = require("./Schema/UserSchema");
 
 const router = express.Router();
 
 router.post("/addpost", async (req, res) => {
   const post = {
-    user_id: "652812f0bf41d828c0b78c60",
+    user_id: req.headers.authorization,
     media: req.body.media_url,
     caption: req.body.caption,
   };
@@ -18,6 +19,26 @@ router.post("/addpost", async (req, res) => {
   }
 });
 
+router.post("/getposts", async (req, res) => {
+  //first check who is logged in or who has opened his profile
+  const whoseProfileIsIt = req.headers.authorization;
+  // find the follwing list of that person
+  const findFollowingList = await User.findById(whoseProfileIsIt);
+  console.log(findFollowingList.following);
+  if (findFollowingList.following.length != 0) {
+    //find the posts added by all the people in following list and return to frontend
+    if (findFollowingList) {
+      findFollowingList.following.push(whoseProfileIsIt);
+      const posts = findFollowingList.following.map(async (elem) => {
+        return await Post.findOne({ user_id: elem });
+      });
+
+      Promise.all(posts).then((response) => {
+        res.status(200).send(response);
+      });
+    }
+  }
+});
 router.post("/likepost", async (req, res) => {
   const postWhichWasLiked = req.body.post_id;
   const userWhoLiked = "652957639a5315d69de60f04";
